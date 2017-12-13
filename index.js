7a27ec9a00418e78fb52bd0147d48e1b875ec6bb
mvoss9000 <mvoss9000@yahoo.com>
2017-12-13 10:36:41 -0500

    work to separate basic parsing from split buffer handling

--- index.js
+++ index.js
@@ -14,10 +14,10 @@
 // ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 // OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-// STATES   - LSB (0x7F) are reserved for token ascii value.  see readme
-// contexts (in array, in object, or none)
+var parse_state = require('./parse_state')
 
-// parse state: before first key, after value, ...
+// STATES   - LSB (0x7F) are reserved for token ascii value.
+// BFK = before first key, B_K = before key, A_V = after value, ...
 var STATES = {
   ARR_BFV: 0x080,
   ARR_B_V: 0x100,
@@ -402,19 +402,19 @@ function _tokenize (init, opt, cb) {
     halted: !cb_continue,
     ecode: ecode,
     src: src,           // src, koff, klim... hold the key and value bytes that can be used to recover from truncation.
-    position: new Position(
-      off,
-      lim,
-      vcount,
-      koff,
-      klim,
-      tok,
-      voff,
-      idx,
-      stack,
-      state0,
-      ecode
-    ),
+    position: parse_state.create({
+      off: off,
+      lim: lim,
+      vcount: vcount,
+      koff: koff,
+      klim: klim,
+      tok: tok,
+      voff: voff,
+      vlim: idx,
+      stack: stack,
+      state: state0,
+      ecode: ecode,
+    }),
   }
 
   var val_str = esc_str(info.src, voff, idx)
@@ -513,99 +513,6 @@ function figure_msg_etok (info, tok, val_str, range, incremental) {
   return { msg: msg, etok: etok }
 }
 
-function pos_str (state, relative) {
-  switch (state) {
-    case STATES.OBJ_BFK: return relative ? 'before first key' : 'first key'
-    case STATES.OBJ_B_K: return relative ? 'before key' : 'key'
-    case STATES.OBJ_A_K: return relative ? 'after key' : 'key'
-    case STATES.ARR_BFV: case STATES.OBJ_BFV: return relative ? 'before first value' : 'first value'
-    case STATES.ARR_B_V: case STATES.OBJ_B_V: return relative ? 'before value' : 'value'
-    case STATES.ARR_A_V: case STATES.OBJ_A_V: return relative ? 'after value' : 'value'
-  }
-}
-
-// Position represents parse position information - both logical and absolute (bytes).  Format (line and column) is
-// not tracked by Position.
-function Position (off, lim, vcount, koff, klim, tok, voff, vlim, stack, state, ecode) {
-  this.off = off
-  this.lim = lim
-  this.vcount = vcount
-  this.koff = koff
-  this.klim = klim
-  this.tok = tok
-  this.voff = voff
-  this.vlim = vlim
-  this.stack = stack
-  this.state = state
-  this.ecode = ecode
-}
-
-Position.prototype = {
-  constructor: Position,
-  description: function (ecode) {
-    var ctx = this.in_arr ? 'in array ' : (this.in_obj ? 'in object ' : '')
-    return ctx + pos_str(this.state, ecode !== END.TRUNC_KEY && ecode !== END.TRUNC_VAL)
-  },
-  get in_arr () { return this.stack[this.stack.length - 1] === 91 },
-  get in_obj () { return this.stack[this.stack.length - 1] === 123 },
-  get parse_state () {
-    var ret = this.stack.map(function (b) { return String.fromCharCode(b) }).join('')
-    var in_obj = this.in_obj
-    var vlen = this.vlim - this.voff
-
-    var klen = 0
-    var gap = 0
-    if (this.koff !== -1) {
-      gap = this.voff - this.klim
-      klen = this.klim - this.koff
-    }
-
-    if (this.ecode === END.TRUNC_KEY) {
-      ret += vlen   // only complete keys are represented by koff..klim.  truncations and other errors are all at voff/vlim
-    } else if (this.ecode === END.TRUNC_VAL ) {
-      if (in_obj) {
-        if (this.state === STATES.ARR_B_V) {
-          ret += vlen
-        } else if (this.state === STATES.OBJ_B_V) {
-          ret += klen + '.' + (gap - 1) + ':' + vlen
-        } else {
-          err('unexpected state for truncated value: ' + this.state)
-        }
-      } else {
-        ret += vlen
-      }
-    } else {
-      switch (this.state) {
-        case STATES.ARR_BFV:
-        case STATES.OBJ_BFK:
-          ret += '-'
-          break
-        case STATES.ARR_B_V:
-        case STATES.OBJ_B_K:
-          ret += '+'
-          break
-        case STATES.ARR_A_V:
-        case STATES.OBJ_A_V:
-          ret += '.'
-          break
-        case STATES.OBJ_A_K:
-          ret += klen + (gap > 0 ? '.' + gap : '') + '.'
-          break
-        case STATES.OBJ_B_V:
-          ret += klen + (gap > 1 ? '.' + (gap - 1) : '') + ':'
-          break
-        default:
-          err('state not handled: ' + this.state)
-      }
-    }
-    return ret
-  },
-  toString: function () {
-    var bytes = this.vlim - this.off
-    var tbytes = this.lim - this.off
-    return this.vcount + '/' + bytes + ':' + tbytes + '/' + this.parse_state
-  }
-}
 
 function esc_str (src, off, lim) {
   var ret = ''
@@ -646,4 +553,5 @@ module.exports = {
   tokenize: tokenize,
   args2str: args2str,
   TOK: TOK,
+  STAATES: STATES,
 }
