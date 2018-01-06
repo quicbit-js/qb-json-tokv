0a5cf15afe51a0b18cdddad2fe0dfb3a33546722
mvoss9000 <mvoss9000@yahoo.com>
2018-01-06 17:39:17 -0500

    make parse state more direct (less translation)

--- index.js
+++ index.js
@@ -148,13 +148,13 @@ function finish_fixed (ps) {
   else                    { ps.pos = pos1; return true }
 }
 
-// function finish_str (ps) {
-//   ps.vlim = skip_str(ps.src, ps.vlim, ps.lim)
-//   var pos1 = POS_MAP[ps.pos | ps.tok]
-//   if (pos1 === 0)         { ps.vlim = ps.vlim < 0 ? -ps.vlim : ps.vlim; ps.tok = TOK.UNEXPECTED;  return false }
-//   if (ps.vlim <= 0)       { ps.vlim = -ps.vlim; ps.trunc = true; if (ps.vlim !== ps.lim) { ps.tok = TOK.BAD_BYT } return false }
-//   else                    { ps.pos = pos1; return true }
-// }
+function finish_str (ps) {
+  ps.vlim = skip_str(ps.src, ps.vlim, ps.lim)
+  var pos1 = POS_MAP[ps.pos | ps.tok]
+  if (pos1 === 0)         { ps.vlim = ps.vlim < 0 ? -ps.vlim : ps.vlim; ps.tok = TOK.UNEXPECTED;  return false }
+  if (ps.vlim <= 0)       { ps.vlim = -ps.vlim; ps.trunc = true; if (ps.vlim !== ps.lim) { ps.tok = TOK.BAD_BYT } return false }
+  else                    { ps.pos = pos1; return true }
+}
 
 function skip_str (src, off, lim) {
   for (var i = off; i < lim; i++) {
@@ -251,33 +251,28 @@ function tokenize (ps, opt, cb) {
 
         case 34:                                          // "    QUOTE
           ps.tok = 115                                    // s for string
-          ps.vlim = skip_str(ps.src, ps.vlim, ps.lim)
-          pos1 = POS_MAP[ps.pos | ps.tok]
-          if (pos1 === 0)         { ps.vlim = ps.vlim < 0 ? -ps.vlim : ps.vlim; ps.tok = TOK.UNEXPECTED;  break main_loop }
-          if (ps.vlim <= 0)       { ps.vlim = -ps.vlim; ps.trunc = true; if (ps.vlim !== ps.lim) { ps.tok = TOK.BAD_BYT } break main_loop }
-          else                    {
-            ps.pos = pos1
+          if (finish_str(ps)) {
             if (ps.pos === OBJ_A_K) { ps.koff = ps.voff; ps.klim = ps.vlim; continue }
-            else { ps.vcount++; break }
-          }
+            else                    { ps.vcount++; break }
+          } else                    { break main_loop }
 
         case 102:                                         // f    false
         case 110:                                         // n    null
         case 116:                                         // t    true
-          if (finish_fixed(ps)) { ps.vcount++; break }
-          else                  { break main_loop }
+          if (finish_fixed(ps))     { ps.vcount++; break }
+          else                      { break main_loop }
 
         case 48:case 49:case 50:case 51:case 52:          // 0-4    digits
         case 53:case 54:case 55:case 56:case 57:          // 5-9    digits
         case 45:                                          // '-'    ('+' is not legal here)
           ps.tok = 100                                    // d for decimal
-          if (finish_dec(ps)) { ps.vcount++; break }
-          else                { break main_loop }
+          if (finish_dec(ps))       { ps.vcount++; break }
+          else                      { break main_loop }
 
         case 91:                                          // [    ARRAY START
         case 123:                                         // {    OBJECT START
           pos1 = pmap[ps.pos | ps.tok]
-          if (pos1 === 0)             { ps.tok = TOK.UNEXPECTED; break main_loop }
+          if (pos1 === 0)           { ps.tok = TOK.UNEXPECTED; break main_loop }
           ps.pos = pos1
           ps.stack.push(ps.tok)
           break
