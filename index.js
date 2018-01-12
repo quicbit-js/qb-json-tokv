3e59cd396d930254d6d4c5ec4bfbe35c5fcc0dbc
mvoss9000 <mvoss9000@yahoo.com>
2018-01-12 17:01:27 -0500

    work on next_src

--- index.js
+++ index.js
@@ -383,7 +383,7 @@ function next_src (ps1, ps2) {
       ps1.tok = ps1.src[ps1.voff]
       switch (ps1.tok) {
         case 102: case 110: case 116:
-          idx = skip_bytes(ps2.src, ps2.vlim, ps2.lim, TOK_BYTES[ps1.tok])
+          idx = skip_bytes(ps2.src, ps2.vlim, ps2.lim, TOK_BYTES[ps1.tok].slice(ps1.vlim - ps1.voff - 1))
           break
         case 34:
           idx = skip_str(ps2.src, ps2.vlim, ps2.lim)
