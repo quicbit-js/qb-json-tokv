49e3fc0e64e461bfaa0e05efde3675d5fe3d7fa7
mvoss9000 <mvoss9000@yahoo.com>
2018-01-13 00:08:38 -0500

    work on next_src

--- index.js
+++ index.js
@@ -400,7 +400,7 @@ function finish_trunc (ps1, ps2) {
     if (idx < 0) {
       // still truncated, expand ps1.src with all of ps2.src
       reset_src(ps1, concat_src(ps1.src, ps1.koff, ps1.lim, ps2.src, ps2.vlim, ps2.lim))
-      ps2.off = ps2.koff = ps2.klim = ps2.voff = ps2.vlim = ps2.lim
+      reset_src(ps2, [])
       return TOK.END
     } else {
       // finished key
